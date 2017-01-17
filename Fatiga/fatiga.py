b0a81683f54c0bc4ab70ed81e1a707e652e30147
Andres Coulson <acoulson@nativapps.mobi>
2017-01-17 16:05:59 -0500

    funcionando errores en espectro

--- Fatiga/fatiga.py
+++ Fatiga/fatiga.py
@@ -1,55 +1,63 @@
 import numpy as np
 import rainflow
-from matplotlib.pylab import hist, show
 import matplotlib.pyplot as plt
 import math
 import Tkinter
 from Tkinter import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from numpy import arange, sin, pi
+from tkFileDialog import askopenfilename
+
+
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.filebase = 0
+        self.btnLoad = Button(master, text="Load", command=self.loadPres).grid(row=1, column=1)
+        self.btnRcc = Button(master, text="RCC Analisys", command=self.rainflow).grid(row=1, column=3)
+        self.btnSlf = Button(master, text="SLF Analisys").grid(row=1, column=4)
+        self.btnReset = Button(master, text="Reset").grid(row=1, column=5)
+        self.btnExit = Button(master, text="Exit", command=quit).grid(row=1, column=6)
+
+        self.file_opt = options = {}
+
+        options['defaultextension'] = '.pres'
+        options['filetypes'] = [('all files', '.*'), ('pressure files', '.pres')]
+        options['initialdir'] = 'C:\\'
+        options['parent'] = master
+        options['title'] = 'Presssure'
+
+    def loadPres(self):
+        filename = askopenfilename(**self.file_opt)
+        if filename:
+            f = open(filename)
+            data = [line.replace("\n", " ")[0:] for line in f.readlines()[0:]]
+
+            all_data = np.zeros(len(data))
+            for i in range(len(data)):
+                all_data[i] = float(data[i])
+            all_data.sort()
+
+            f.close()
+
+            plt.hist(all_data, bins=int(math.sqrt(len(all_data))), normed=True)
+
+            self.filebase = all_data
+
+            # falta el espectro de frecuencia
+
+            plt.show()
+
+        else:
+            m = Message(self.master, text="Error open file")
+
+    def rainflow(self):
+        print rainflow.extract_cycles(self.filebase)
 
 
 ventanaPrincipal = Tk()
+app = App(ventanaPrincipal)
+ventanaPrincipal.wm_title("Embedding in TK")
 ventanaPrincipal.geometry("800x500")
-
-def ejecutar(f): ventanaPrincipal.after(200,f)
-
-btnLoad = Button(ventanaPrincipal, text="Load").grid(row=1, column=1)
-btnRcc = Button(ventanaPrincipal, text="RCC Analisys").grid(row=1, column=3)
-btnSlf = Button(ventanaPrincipal, text="SLF Analisys").grid(row=1, column=4)
-btnReset = Button(ventanaPrincipal, text="Reset").grid(row=1, column=5)
-btnExit = Button(ventanaPrincipal, text="Exit").grid(row=1, column=6)
-
-
 ventanaPrincipal.mainloop()
-
-
-# f = open('25_547_31.pres')
-#
-# data = [line.replace("\n", " ")[0:] for line in f.readlines()[0:]]
-#
-# all_data =  np.zeros(len(data))
-# for i in range(len(data)):
-#     all_data[i] = float(data[i])
-# all_data.sort()
-#
-# f.close()
-#
-# plt.hist(all_data, bins= int(math.sqrt(len(all_data))) , normed=True)
-# plt.show()
-#
-#
-# dt = 0.01
-# Fs = 1/dt
-# t = np.arange(0, 10, dt)
-# nse = np.random.randn(len(t))
-# r = np.exp(-t/0.05)
-#
-# cnse = np.convolve(nse, r)*dt
-# cnse = cnse[:len(t)]
-# s = 0.1*np.sin(2*np.pi*t) + cnse
-#
-#
-#
-#
-# plt.angle_spectrum(s, Fs=Fs)
-#
-# plt.show()
