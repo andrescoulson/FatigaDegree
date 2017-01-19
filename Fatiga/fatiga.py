6de5718b0d88b6c85302e833ae2f916cc3d4bd19
Andres Coulson <acoulson@nativapps.mobi>
2017-01-19 15:49:49 -0500

    arreglando raiflow

--- Fatiga/fatiga.py
+++ Fatiga/fatiga.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import numpy as np
 import rainflow
 import matplotlib.pyplot as plt
@@ -15,7 +16,7 @@ class App:
         self.master = master
         self.filebase = 0
         self.btnLoad = Button(master, text="Load", command=self.loadPres).grid(row=1, column=1)
-        self.btnRcc = Button(master, text="RCC Analisys", command=self.rainflow).grid(row=1, column=3)
+        self.btnRcc = Button(master, text="RCC Analisys", command=self.onRccBtn).grid(row=1, column=3)
         self.btnSlf = Button(master, text="SLF Analisys").grid(row=1, column=4)
         self.btnReset = Button(master, text="Reset").grid(row=1, column=5)
         self.btnExit = Button(master, text="Exit", command=quit).grid(row=1, column=6)
@@ -37,11 +38,10 @@ class App:
             all_data = np.zeros(len(data))
             for i in range(len(data)):
                 all_data[i] = float(data[i])
-            all_data.sort()
 
             f.close()
 
-            plt.hist(all_data, bins=int(math.sqrt(len(all_data))), normed=True)
+            plt.hist(all_data, bins=int(math.sqrt(len(all_data))), normed=False)
 
             self.filebase = all_data
 
@@ -54,21 +54,23 @@ class App:
 
     def findext(self):
         matrix_in = self.filebase
-        #extrae los máximos y ptos de inflexion
+        # extrae los máximos y ptos de inflexion
         wi = self.diff(matrix_in)
-        data = []
+        data = np.zeros(len(matrix_in))
         data[0] = matrix_in[0]
         no = 0
-        current = 0  # tamaño actual del resultado
-        for i in range(len(matrix_in) - 2):
-            if wi[i] * wi[i + 1] <= 0:
-                no = no + 1
+        current = len(matrix_in)  # tamaño actual del resultado
+        for i in range(current - 2):
+            if (wi[i] * wi[i + 1]) <= 0:
+                no += 1
                 data[no] = matrix_in[i + 1]
 
         data[no + 1] = matrix_in[len(matrix_in) - 1]
-        no = no + 2
+        no += 2
+        data = data[:no]
 
-        #elimina puntos de inflexión
+        # print "primer no ", no
+        # elimina puntos de inflexión
         if no != 0:
             current = no
             no = 0
@@ -77,12 +79,13 @@ class App:
 
         wi = self.diff(data)
 
-        for i in range(current-1):
+        for i in range(current - 2):
             if ~((wi[i] == 0) & (wi[i + 1] == 0)):
-                no = no + 1
+                no += 1
                 data[no] = matrix_in[i + 1]
 
-        no = no + 1  # se agrega por el ultimo
+        no += 1  # se agrega por el ultimo
+        data = data[:no]
 
         # retira los repetidos
         if no != 0:
@@ -90,11 +93,10 @@ class App:
             no = 0
 
         matrix_in = data
-
-        for i in range(current):
-            if ~ matrix_in[i] == matrix_in[i + 1]:
+        for i in range(current - 1):
+            if ~ (matrix_in[i] == matrix_in[i + 1]):
                 data[no] = matrix_in[i]
-                no = no + 1
+                no += 1
 
         # extrae los maximos
 
@@ -105,27 +107,90 @@ class App:
         if len(data) > 2:
             wi = self.diff(data)
 
-            for i in range(current):
+            for i in range(current-1):
                 if wi[i] * wi[i + 1] < 0:
-                    no = no + 1
+                    no += 1
                     data[no] = matrix_in[i + 1]
-        #almacena datos en MATRIX_OUT
-        matrix_out = data
+        # almacena datos en MATRIX_OUT
 
+        data = data[:no]
+        matrix_out = data
         return matrix_out
 
     def diff(self, filebase):
-        wi = np.zeros(len(filebase))
+        wi = []
         for i in range(len(filebase) - 1):
-            wi[i] = filebase[i + 1] - filebase[i]
-
+            wi.append(filebase[i + 1] - filebase[i])
 
         return wi
 
-    def rainflow(self, dataIn, filebase):
-        wi = []
+    def rainflow(self, datain):
+        # inicializando variables
+        data = datain
+        tot_num = len(data)
+        data_out = np.zeros(len(data)*3)
+        j = -1
+        cNr = 1
+        k = 0
+        a = np.zeros(len(data))
+        ampl = 0
+        mean = 0
+        current_vector = 0
+        current_vector_salida = 0
+        # fin de inicializacion
 
+        for i in range(tot_num):
+            j += 1
+            a[j] = data[current_vector]
+            current_vector += 1
+            while j >= 2 and (math.fabs(a[j - 1] - a[j - 2]) <= math.fabs(a[j] - a[j - 1])):
+                ampl = math.fabs((a[j - 1] - a[j - 2]) / 2)
+                if j == 0:
+                    nada = 0
+                elif j == 1:
+                    nada = 0
+                elif j == 2:
+                    mean = (a[0] + a[1]) / 2
+                    a[0] = a[1]
+                    a[1] = a[2]
+                    j = 1
+                    if ampl > 0:
+                        data_out[current_vector_salida] = ampl
+                        current_vector_salida += 1
+                        data_out[current_vector_salida] = mean
+                        current_vector_salida += 1
+                        data_out[current_vector_salida] = 0.50
+                        k += 1
+                else:
+                    mean = (a[j - 1] + a[j - 2]) / 2
+                    a[j - 2] = a[j]
+                    j -= 2
+                    if ampl > 0:
+                        data_out[current_vector_salida] = ampl
+                        current_vector_salida += 1
+                        data_out[current_vector_salida] = mean
+                        current_vector_salida += 1
+                        data_out[current_vector_salida] = 1.00
+                        cNr += 1
+                        k += 1
+                for l in range(j - 1):
+                    ampl = math.fabs(a[l] - a[l + 1]) / 2
+                    mean = (a[l] + a[l + 1]) / 2
+                    if ampl > 0:
+                        data_out[current_vector_salida] = ampl
+                        current_vector_salida += 1
+                        data_out[current_vector_salida] = mean
+                        current_vector_salida += 1
+                        data_out[current_vector_salida] = 0.50
+                        k += 1
+                col = k
+                print len(data_out)
+                return data_out, col
 
+    def onRccBtn(self):
+        dataout = self.findext()
+        fatiga, col = self.rainflow(dataout)
+        print fatiga
 
 
 ventanaPrincipal = Tk()
